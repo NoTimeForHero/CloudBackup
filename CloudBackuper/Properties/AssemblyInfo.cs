f83b9fd8673d132186656b3be99f1788ec769772
NoTimeForHero <notimeforhero@yandex.ru>
2022-06-29 01:24:06 +0300

    Обновление номера версии

--- CloudBackuper/Properties/AssemblyInfo.cs
+++ CloudBackuper/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.2206.1")]
-[assembly: AssemblyFileVersion("3.0.2206.1")]
+[assembly: AssemblyVersion("3.0.2906.1")]
+[assembly: AssemblyFileVersion("3.0.2906.1")]
